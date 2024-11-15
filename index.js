e6fe189d223d5f9177d4f847b2b09927f75774f5
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-15 16:26:33 +1100

    use env variables

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import { getGraphClient } from "./lib/msAuth.js";
 import { getExcelData, updateExcelData } from "./lib/sheets.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const client = await getGraphClient();
 
 // Replace with your OneDrive file ID, sheet name, and user ID
-const workbookId = "01FNQELGBWKBKRZQZJYZFLPYTIA4IS3HB7"; // Use this ID in your API calls
-const sheetName = "test";
-const userId = "c9641b08-94b7-4988-99a9-c2ddad0268d4"; // Specify the user ID here
+const workbookId = process.env.WORKBOOK_ID;
+const sheetName = process.env.SHEET_NAME;
+const userId = process.env.USER_ID;
 
 // Retrieve existing data only once
 const data = await getExcelData(client, userId, workbookId, sheetName);
 
 // Example of updating data
-const rangeToUpdate = "C6:D6"; // Specify the range to update
-const newValues = [["Updated Value 1", "Updated Value 2"]]; // New data to update
-await updateExcelData(
-  client,
-  userId,
-  workbookId,
-  sheetName,
-  rangeToUpdate,
-  newValues
-);
+// const rangeToUpdate = "C6:D6"; // Specify the range to update
+// const newValues = [["Updated Value 1", "Updated Value 2"]]; // New data to update
+// await updateExcelData(
+//   client,
+//   userId,
+//   workbookId,
+//   sheetName,
+//   rangeToUpdate,
+//   newValues
+// );
