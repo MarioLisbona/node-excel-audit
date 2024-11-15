ef1725151526933fce015b24e250909c6612259f
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-15 16:49:31 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { getGraphClient } from "./lib/msAuth.js";
-import { getExcelData, updateExcelData } from "./lib/sheets.js";
+import { getExcelData } from "./lib/sheets.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,16 +12,9 @@ const sheetName = process.env.SHEET_NAME;
 const userId = process.env.USER_ID;
 
 // Retrieve existing data only once
-const data = await getExcelData(client, userId, workbookId, sheetName);
-
-// Example of updating data
-// const rangeToUpdate = "C6:D6"; // Specify the range to update
-// const newValues = [["Updated Value 1", "Updated Value 2"]]; // New data to update
-// await updateExcelData(
-//   client,
-//   userId,
-//   workbookId,
-//   sheetName,
-//   rangeToUpdate,
-//   newValues
-// );
+const updatedRfiCellData = await getExcelData(
+  client,
+  userId,
+  workbookId,
+  sheetName
+);
