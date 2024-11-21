853f129a8a742b49f1406dacf8471de8dd95d565
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-21 16:29:38 +1100

    Use getFileIdByName

--- emailRFISheet.js
+++ emailRFISheet.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import { getAccessToken } from "./lib/msAuth.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { getFileIdByName } from "./returnFileIdfromFileName.js";
 dotenv.config();
 
 // Assuming you have a working getAccessToken function
@@ -67,15 +68,17 @@ const sendEmailWithAttachment = async (filePath) => {
   }
 };
 
-const main = async () => {
+export const emailRfiToClient = async () => {
   try {
     const accessToken = await getAccessToken(); // Use your existing function
-    const fileId = process.env.WORKBOOK_ID_TO_EMAIL; // Replace with your actual file ID
+    const fileId = await getFileIdByName(
+      process.env.ONEDRIVE_ID,
+      process.env.WORKSHEET_NAME_TO_EMAIL
+    ); // Replace with your actual file ID
+    console.log(fileId);
     const filePath = await downloadFile(accessToken, fileId);
     await sendEmailWithAttachment(filePath);
   } catch (error) {
     console.error("Error: ", error);
   }
 };
-
-main();
