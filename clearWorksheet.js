9cfa15ca18c49e33430921bb2fafd73a54ce767c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-26 12:18:51 +1100

    Use ranges array to not delete heading cell data

--- clearWorksheet.js
+++ clearWorksheet.js
@@ -5,7 +5,7 @@ export const clearWorksheetRange = async (
   userId,
   workbookId,
   worksheetName,
-  rangeAddress
+  ranges
 ) => {
   try {
     const client = await getGraphClient(); // Initialize the Graph client
@@ -14,15 +14,13 @@ export const clearWorksheetRange = async (
       applyTo: "Contents", // Specify that we want to clear the contents
     };
 
-    // Construct the API endpoint using the userId, workbookId, and worksheetName
-    const apiEndpoint = `/users/${userId}/drive/items/${workbookId}/workbook/worksheets/${worksheetName}/range(address='${rangeAddress}')/clear`;
-
-    // Make the API call to clear the range
-    await client.api(apiEndpoint).post(clear);
-
-    console.log(
-      `Cleared contents of range ${rangeAddress} in worksheet ${worksheetName}.`
-    );
+    for (const range of ranges) {
+      const apiEndpoint = `/users/${userId}/drive/items/${workbookId}/workbook/worksheets/${worksheetName}/range(address='${range}')/clear`;
+      await client.api(apiEndpoint).post(clear);
+      console.log(
+        `Cleared contents of range ${range} in worksheet ${worksheetName}.`
+      );
+    }
   } catch (error) {
     console.error("Error clearing worksheet range:", error.message);
     console.error("Full error details:", error);
@@ -32,7 +30,7 @@ export const clearWorksheetRange = async (
 const userId = process.env.USER_ID;
 const workbookId = "01FNQELGG3VHA4YXYQMZCKASTZIB7R46IS";
 const sheetName = "delete-spreadsheet";
-const rangeAddress = "A7:F100";
+const ranges = ["A7:F16", "A18:F100"]; // Array of ranges to clear
 
 // Example usage
-// clearWorksheetRange(userId, workbookId, sheetName, rangeAddress);
+clearWorksheetRange(userId, workbookId, sheetName, ranges);
