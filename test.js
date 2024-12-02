aa33b8927c310fdeeb518fe81e3eb9e8220c8c1f
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-12-02 13:15:13 +1100

    testing AzureGpt queries

--- test.js
+++ test.js
@@ -1,21 +1,4 @@
-import { getGraphClient } from "./lib/msAuth.js";
+import { azureGptQuery } from "./lib/azureGpt.cjs";
 
-// Function to write test data to a worksheet
-export const writeTestDataToWorksheet = async () => {
-  const graphClient = await getGraphClient();
-
-  const testData = [
-    ["Header1", "Header2"],
-    ["Row1Col1", "Row1Col2"],
-  ];
-
-  await graphClient
-    .api(
-      `/drives/${process.env.ONEDRIVE_ID}/items/${process.env.NEW_TEST_SPREADSHEET_ID}/workbook/worksheets/${process.env.NEW_TEST_WORKSHEET_NAME}/range(address='A1:B2')`
-    )
-    .patch({
-      values: testData,
-    });
-};
-
-writeTestDataToWorksheet();
+const response = await azureGptQuery("What is the capital of France?");
+console.log(response);
